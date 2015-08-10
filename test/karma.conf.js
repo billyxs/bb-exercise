906d0b2dfafb3a7b5550030966631ee091a24295
billyxs <billymont@gmail.com>
2015-08-09 23:48:07 -0700

    Update app and test to use angular cdn

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -22,9 +22,9 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      'bower_components/angular/angular.js',
-      'bower_components/angular-route/angular-route.js',
-      'bower_components/angular-mocks/angular-mocks.js',
+      'https://code.angularjs.org/1.4.3/angular.min.js',
+      'https://code.angularjs.org/1.4.3/angular-route.min.js',
+      'https://code.angularjs.org/1.4.3/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
       "test/mock/**/*.js",
